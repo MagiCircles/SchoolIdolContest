11d8743253f2bfb67661fc2e9299e73f5249117a
db0company <db0company@gmail.com>
2015-03-14 19:36:17 -0700

    Design past contests page + fix what happens if there are no contests

--- schoolidolcontest/views.py
+++ schoolidolcontest/views.py
@@ -265,7 +265,7 @@ def contest_vote_view(request):
     return {
         'contest': contest,
         'current_contest': contest,
-        'title': contest.title,
+        'title': contest.name,
         'cards': cards,
         'begin': contest.begin,
         'end': contest.end,
